9c5dac4eff369f85065aa24bce1e19c2145b3729
Dong Xie <xiedong1993@gmail.com>
2013-11-02 00:46:00 +0800

    Finish with Task 6 about Boat, Ready to tag.

--- nachos-sjtu/src/nachos/threads/Boat.java
+++ nachos-sjtu/src/nachos/threads/Boat.java
@@ -1,5 +1,7 @@
 package nachos.threads;
 
+import java.util.LinkedList;
+
 import nachos.ag.BoatGrader;
 
 public class Boat {
@@ -24,19 +26,44 @@ public class Boat {
 		bg = b;
 
 		// Instantiate global variables here
-
+		lock = new Lock();
+		cond = new Condition(lock);
+		adultsLeft = adults;
+		childrenLeft = children;
+		boatAtOahu = true;
+		HasPilot = false;
+		over = false;
+		
 		// Create threads here. See section 3.4 of the Nachos for Java
 		// Walkthrough linked from the projects page.
-
-		Runnable r = new Runnable() {
-			public void run() {
-				SampleItinerary();
-			}
-		};
-		KThread t = new KThread(r);
-		t.setName("Sample Boat Thread");
-		t.fork();
-		t.join();
+		LinkedList<KThread> threads = new LinkedList<KThread>();
+		
+		for (int i = 0; i < adults; ++i) {
+			KThread t = new KThread(new Runnable(){
+				@Override
+				public void run() {
+					AdultItinerary();
+				}
+			});
+			t.setName("Adult Thread" + i);
+			threads.add(t);
+			t.fork();
+		}
+		
+		for (int i = 0; i < children; ++i) {
+			KThread t = new KThread(new Runnable(){
+				@Override
+				public void run() {
+					ChildItinerary();
+				}
+			});
+			t.setName("Child Thread" + i);
+			threads.add(t);
+			t.fork();
+		}
+		
+		for (KThread t: threads)
+			t.join();
 	}
 
 	static void AdultItinerary() {
@@ -46,9 +73,69 @@ public class Boat {
 		 * bg.AdultRowToMolokai(); indicates that an adult has rowed the boat
 		 * across to Molokai
 		 */
+		
+		lock.acquire();
+		
+		while (!(boatAtOahu && childrenLeft <= 1))
+			cond.sleep();
+		
+		boatAtOahu = false;
+		bg.AdultRowToMolokai();
+		adultsLeft--;
+		if (adultsLeft == 0 && childrenLeft == 0)
+			over = true;
+		
+		cond.wakeAll();
+		//System.out.println("1 adult to Molokai");
+		
+		lock.release();
 	}
 
 	static void ChildItinerary() {
+		boolean AtOahu = true;
+		lock.acquire();
+		
+		while (!over) {
+			if (boatAtOahu && AtOahu) {
+				if (childrenLeft >= 2) {
+					if (!HasPilot) { //Take one child as pilot
+						HasPilot = true;
+						AtOahu = false;
+					} else {
+						boatAtOahu = false;
+						bg.ChildRowToMolokai();
+						bg.ChildRideToMolokai();
+						AtOahu = false;
+						HasPilot = false;
+						childrenLeft -= 2;
+						if (adultsLeft == 0 && childrenLeft == 0)
+							over = true;
+					
+						cond.wakeAll();
+						//System.out.println("2 child to Molokai");
+					}
+				} else if (childrenLeft == 1 && adultsLeft == 0) {
+					boatAtOahu = false;
+					bg.ChildRowToMolokai();
+					AtOahu = false;
+					childrenLeft--;
+					over = true;
+					
+					cond.wakeAll();
+					//System.out.println("1 child to Molokai");
+				} else cond.sleep();
+			} else if (!boatAtOahu && !AtOahu) {
+				bg.ChildRowToOahu();
+				boatAtOahu = true;
+				AtOahu = true;
+				childrenLeft += 1;
+				
+				cond.wakeAll();
+				//System.out.println("1 child to Oahu");
+			} else cond.sleep();
+		}
+		
+		lock.release();
 	}
 
 	static void SampleItinerary() {
@@ -63,5 +150,13 @@ public class Boat {
 		bg.AdultRideToMolokai();
 		bg.ChildRideToMolokai();
 	}
-
+	
+	
+	static private Lock lock;
+	static private Condition cond;
+	
+	static private int adultsLeft, childrenLeft;
+	static private boolean over;
+	static private boolean boatAtOahu;
+	static private boolean HasPilot;
 }
